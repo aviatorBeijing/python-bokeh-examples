6413b69ed3634c7b420be7640e91e6d562dc94dd
jun.ma <junma75@gmail.com>
2018-03-19 18:01:25 +0800

    .

--- bokeh-lapd-party-calls.py
+++ bokeh-lapd-party-calls.py
@@ -8,8 +8,9 @@ import datetime
 import urllib
  
 from bokeh.plotting import ColumnDataSource, figure, output_file
-from bokeh.models import HoverTool
+from bokeh.models import HoverTool, Circle
 from collections import OrderedDict
+
  
 # Read in our data. We've aggregated it by date already, so we don't need to worry about paging
 query = ("https://data.lacity.org/resource/mgue-vbsx.json?"
@@ -57,30 +58,40 @@ source = ColumnDataSource(
 )
     
 output_file('C:\\Users\\U6033615\\Downloads\\all-las-parties.html')
- 
-TOOLS = "hover"
 
-p=figure(
+TOOLS = 'box_zoom,box_select,reset,hover'
+fig=figure(
     title='\"Party\" Disturbance Calls in LA', 
     x_range=weeks, 
     y_range=list(reversed(days)),
     tools=TOOLS)
-p.plot_width=900
-p.plot_height = 400
-p.toolbar_location='left'
+fig.plot_width=900
+fig.plot_height = 400
+fig.toolbar_location='left'
 
-p.rect(raw_data["week"], raw_data["day_of_week"], width=1, height=1,color=color, line_color=None)
+# For tooltips [TODO]
+#fig.annulus(x='day_of_week', y='week', inner_radius=0.29, outer_radius=0.295,source=source)
+#circle = Circle(x='day_of_week', y='week', radius=0.29, fill_color='#e9f1f8')
+#circle_renderer = fig.add_glyph(source, circle)    
 
-p.grid.grid_line_color = None
-p.axis.axis_line_color = None
-p.axis.major_tick_line_color = None
-p.axis.major_label_text_font_size = "10pt"
-p.axis.major_label_standoff = 0
-p.xaxis.major_label_orientation = np.pi/3
+# Draw rect bars with color
+fig.rect(raw_data["week"], raw_data["day_of_week"], width=1, height=1,color=color, line_color=None)
 
-hover = p.select(dict(type=HoverTool))
-hover.tooltips = OrderedDict([
+fig.grid.grid_line_color = None
+fig.axis.axis_line_color = None
+fig.axis.major_tick_line_color = None
+fig.axis.major_label_text_font_size = "5pt"
+fig.axis.major_label_standoff = 0
+fig.xaxis.major_label_orientation = np.pi/3
+
+#hover = fig.select(dict(type=HoverTool))
+#hover.tooltips = OrderedDict([
+#    ('parties', '@parties'),
+#])
+
+tooltips = OrderedDict([
     ('parties', '@parties'),
 ])
-
-show(p) # show the plot
+#fig.add_tools( HoverTool(tooltips=tooltips, renderers=[circle_renderer]))
+    
+show(fig) # show the plot
